4e2a09f0f7a0c9f4d761ea28cf36ca8b42f82f95
jsDobromir <dobrganch89@gmail.com>
2024-10-30 10:42:25 +0200

    fixing errors in myComp.js

--- force-app/main/default/lwc/myComp/myComp.js
+++ force-app/main/default/lwc/myComp/myComp.js
@@ -2,8 +2,10 @@ import { LightningElement } from "lwc";
 
 export default class MyComp extends LightningElement {
     connectedCallback() {
+        var c = "this should be fixed";
         var b = "bad format";
-        return b;
+        let resStr = b + ", " + c; //both vars are used now
+        return resStr;
     }
 
     badFormattedMethod() {
