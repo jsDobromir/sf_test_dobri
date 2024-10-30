2be440d1dd564e92f5a96f9969750a4e05244e8f
jsDobromir <dobrganch89@gmail.com>
2024-10-30 11:04:22 +0200

    fixing error in myComp.js

--- force-app/main/default/lwc/myComp/myComp.js
+++ force-app/main/default/lwc/myComp/myComp.js
@@ -4,8 +4,8 @@ export default class MyComp extends LightningElement {
     connectedCallback() {
         var c = "this should be fixed";
         var b = "bad format";
-        const resStr = c + ", " + b;
-        return resStr;
+        const retStr = c + ", " + b;
+        return retStr;
     }
 
     badFormattedMethod() {
