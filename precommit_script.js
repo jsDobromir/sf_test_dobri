581af2cac317a0f8297945263a971a466380e4b4
Dobromir Ganchev <dobrganch89@gmail.com>
2024-11-20 18:25:50 +0200

    adding

--- precommit_script.js
+++ precommit_script.js
@@ -56,6 +56,7 @@ exec(
             exec(testCommand, (error, stdout, stderr) => {
                 console.log(stdout);
                 const isWarning = stderr && stderr.includes("Warning:");
+                console.log(stderr);
                 if ((error || (stderr && !isWarning)) && !isWarning) {
                     console.error("\x1b[31m", error || stderr);
                     process.exit(1);
