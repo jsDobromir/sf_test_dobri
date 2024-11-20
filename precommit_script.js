39ff86f6eca6e5ff0b1521127b4723d504ffa4d4
Dobromir Ganchev <dobrganch89@gmail.com>
2024-11-20 17:59:09 +0200

    adding

--- precommit_script.js
+++ precommit_script.js
@@ -17,9 +17,12 @@ exec(
             process.exit(0);
         }
         const errorsArray = new Array();
+        const testClasses = new Set();
         for (const file of modifiedFiles) {
             if (file.includes("/classes/")) {
                 const className = file.split("/classes/")[1];
+                const classExt = className.split(".")[1];
+                if (classExt != "cls" || className.includes("Test")) continue;
                 const classNameTest =
                     className.split(".")[0] +
                     "Test" +
@@ -30,18 +33,36 @@ exec(
                     classPath,
                     classNameTest
                 );
+                testClasses.add(fullClassPathTest);
                 console.log(fullClassPathTest);
-                if (!fs.existsSync(fullClassPathTest)) {
+                const fileExists = fs.existsSync(fullClassPathTest);
+                console.log(fileExists);
+                if (!fileExists) {
                     errorsArray.push(
-                        `Error, test class for class: ${className} does not exist`
+                        `Error: test class for class: ${className} does not exist`
                     );
                 }
             }
         }
         if (errorsArray.length > 0) {
             for (const error of errorsArray) {
-                console.log("\x1b[31m", error);
+                console.error("\x1b[31m", error);
             }
+            process.exit(1);
+        }
+
+        if (testClasses.size > 0) {
+            let testClassesArray = Array.from(testClasses);
+            const testCommand = `sfdx force:apex:test:run --classnames ${testClassesArray.join(",")} --resultformat human --synchronous`;
+            exec(testCommand, (testError, testStdOut, testStdErr) => {
+                console.log(testStdOut);
+                if (testError || testStdErr) {
+                    console.error("\x1b[31m", testError || testStdErr);
+                    process.exit(1);
+                }
+                process.exit(0);
+            });
+        } else {
             process.exit(0);
         }
     }
